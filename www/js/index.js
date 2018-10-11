7e537c48762e3d4d24bf45f51bebea32c025972f
SSJAfroGuy <37389192+SSJAfroGuy@users.noreply.github.com>
2018-10-11 13:48:16 +0100

    I added more events, havent tested the last thigny, whoops
    yay for adding stuff

--- www/js/index.js
+++ www/js/index.js
@@ -2,6 +2,8 @@ var paused_count =0;
 var resumed_count = 0;
 var launched_count = 0;
 var Total_Actions = 0;
+var Total_Key_Press = 0;
+var Touch_count = 0;
 
 document.addEventListener("deviceready", onDeviceReady, false);
 		
@@ -11,6 +13,7 @@ function updateDisplay() {
 	$("#resumed").text("Application paused, you should unpause senpai!: " + paused_count);
 	$("#paused").text("Application resumed, damn right you resumed this sexy ass application: " + resumed_count);
 	$("#TotalActions").text("Total number of actions you've performed in this session: " + Total_Actions);
+	$("#TotalKeyPress").text("Total Key Presses: " + Total_Key_Press );
 }
 
 
@@ -20,6 +23,8 @@ function updateDisplay() {
 	
 	document.addEventListener("resume", onResume, false);
 	document.addEventListener("pause", onPause, false);
+	document.addEventListener("key", onkeypress, false);
+	document.addEventListener("touch", onTouch, false);
 	
 	launched_count++;
 	Total_Actions++;
@@ -47,3 +52,22 @@ function updateDisplay() {
 	    
 	alert("reZOOOOOOOOOOOOOM");
     }
+	
+	function onKey() {
+		
+		Total_Key_Press++;
+		Total_Actions++;
+		updateDisplay();
+		
+		alert("OMG YOU PRESSED A KEY, WTF, YOU TRYING TO BREAK THIS APP");
+	}
+	
+	function onTouch() {
+		
+		Touch_count++;
+		Total_Actions++;
+		updateDisplay();
+		
+		alert("stop touching me :O >_< ");
+		
+	}
