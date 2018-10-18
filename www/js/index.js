65fbf3c9841e73da5627e33fbb6e0c4a129432c4
SSJAfroGuy <37389192+SSJAfroGuy@users.noreply.github.com>
2018-10-18 14:12:06 +0100

    alert box value changing
    an alert box shoujld pop up, the choice changes the value

--- www/js/index.js
+++ www/js/index.js
@@ -8,23 +8,15 @@ var key = "pen";
 var value = "blue";
 window.localStorage.setItem(key, value);
 
-var key = "phones";
-var value = 2;
-window.localStorage.setItem(key, value);
-
 document.addEventListener("deviceready", onDeviceReady, false);
 		
 	
 function updateDisplay() 
 {
 	$("#launched").text("Application launched so far into the air, it's literally a star now: " + launched_count);
-	
-	var key = “pen”;
-	var value = window.localStorage.getItem(key);
+
 	$("#resumed").text("I'm actually telling you the value for pen " + value);
 	
-	var key = "phones";
-	var value = window.localStorage.getItem(key);
 	$("#paused").text("Application resumed, damn right you resumed this sexy ass application: " + value);
 	
 	$("#TotalActions").text("Total number of actions you've performed in this session: " + Total_Actions);	
@@ -34,36 +26,47 @@ function updateDisplay()
 // device APIs are available
 //
     function onDeviceReady() 
-	{
-	
-	document.addEventListener("resume", onResume, false);
-	document.addEventListener("pause", onPause, false);
-	
-	launched_count++;
-	Total_Actions++;
-	updateDisplay();
-	    
-	alert("device ready to rumble!!!");
+	{	
+		document.addEventListener("resume", onResume, false);
+		document.addEventListener("pause", onPause, false);
+		
+		launched_count++;
+		Total_Actions++;
+		updateDisplay();
+			
+		alert("device ready to rumble!!!");
     }
 
 
     function onPause() 
-	{
-	
-	paused_count++;
-	Total_Actions++;
-	updateDisplay();
-	    
-	alert("paused like a bad movie #bestjoke2018");
+	{		
+		paused_count++;
+		Total_Actions++;
+		updateDisplay();
+			
+		alert("paused like a bad movie #bestjoke2018");
     }
 	
 
     function onResume() 
 	{
 		
-	resumed_count++;
-	Total_Actions++;
-	updateDisplay();
-	    
-	alert("reZOOOOOOOOOOOOOM");
+		resumed_count++;
+		Total_Actions++;
+		updateDisplay();
+			
+		alert("reZOOOOOOOOOOOOOM");
+	
+		if (confirm("Press a button!")) 
+		{
+		txt = "You pressed OK!";
+		var value = "Red";
+		}
+		
+		else 
+		{
+		txt = "You pressed Cancel!";
+		var value = "Green";
+		}
+	
     }
