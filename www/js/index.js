28e1088ed85b8a9ad9ca4d0aefbd5476d485bc26
SSJAfroGuy <37389192+SSJAfroGuy@users.noreply.github.com>
2018-10-18 14:20:04 +0100

    eh
    asid

--- www/js/index.js
+++ www/js/index.js
@@ -13,13 +13,13 @@ document.addEventListener("deviceready", onDeviceReady, false);
 	
 function updateDisplay() 
 {
-	$("#launched").text("Application launched so far into the air, it's literally a star now: " + launched_count);
+	//$("#launched").text("Application launched so far into the air, it's literally a star now: " + launched_count);
 
-	$("#resumed").text("I'm actually telling you the value for pen " + var value = window.localStorage.getItem(key););
+	$("#resumed").text("The value for the key pen " + value);
 	
-	$("#paused").text("Application resumed, damn right you resumed this sexy ass application: " + value);
+	//$("#paused").text("Application resumed, damn right you resumed this sexy ass application: " + value);
 	
-	$("#TotalActions").text("Total number of actions you've performed in this session: " + Total_Actions);	
+	//$("#TotalActions").text("Total number of actions you've performed in this session: " + Total_Actions);	
 }
 
 
@@ -59,13 +59,13 @@ function updateDisplay()
 	
 		if (confirm("Press a button!")) 
 		{
-		txt = "You pressed OK!";
+		txt = value;
 		var value = "Red";
 		}
 		
 		else 
 		{
-		txt = "You pressed Cancel!";
+		txt = value;
 		var value = "Green";
 		}
 	
